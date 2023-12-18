41b8dcb3181adfd5d42fc414d7dc8f4e98635e89
Ivan Sholana <ivansholana@gmail.com>
2023-12-18 22:59:04 +0700

    feat : add profile

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,5 +1,39 @@
+import { StyleSheet, View } from "react-native";
+import AppBarComponent from "../components/AppBar/AppBarComponents";
+import {
+  ProfileContainer,
+  ProfileContent,
+} from "../containers/Profile/ProfileContainer";
+
 const ProfileScreen = () => {
-  return;
+  return (
+    <>
+      <AppBarComponent content={<ProfileContainer />} />
+      <View style={styles.content}>
+        <ProfileContent />
+      </View>
+    </>
+  );
 };
+
+const styles = StyleSheet.create({
+  logo: {
+    marginTop: 20,
+    marginBottom: 10,
+    marginHorizontal: 10,
+  },
+  container: { flexDirection: "row", justifyContent: "space-between" },
+  buttoncontainer: {
+    flexDirection: "row",
+    width: 100,
+    alignItems: "center",
+    justifyContent: "space-around",
+  },
+  content: {
+    flex: 1,
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+  },
+});
 
 export default ProfileScreen;
