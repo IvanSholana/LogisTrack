9666b91c1883a112c3044207c22ffd32d19011c7
Ivan Sholana <ivansholana@gmail.com>
2023-12-18 21:55:32 +0700

    fix : fixed status component bug

--- components/Status/StatusComponent.js
+++ components/Status/StatusComponent.js
@@ -1,11 +1,20 @@
-import { Text } from "react-native";
+import { StyleSheet, Text } from "react-native";
 
 const StatusComponent = ({ status, color }) => {
   return (
-    <>
-      <Text style={{ color }}>{status}</Text>
-    </>
+    <Text style={[styles.status, { backgroundColor: color }]}>{status}</Text>
   );
 };
+
+const styles = StyleSheet.create({
+  status: {
+    alignSelf: "center",
+    textAlign: "center",
+    width: 80,
+    paddingVertical: 8,
+    borderRadius: 20,
+    color: "white",
+  },
+});
 
 export default StatusComponent;
