1c46b6363aa24c0a9ca7e89bca6f49665b9e1eca
MuammarFaridz <muammarfaridz99@gmail.com>
2023-12-24 21:06:40 +0700

    modified:   App.js

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Image } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
