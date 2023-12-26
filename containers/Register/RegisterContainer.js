4a8711306b1fd3a00c10af215754e3cf899732cf
mharits-s <mharits.beyourself@gmail.com>
2023-12-26 18:35:31 +0700

    modified:   containers/Register/RegisterContainer.js

--- containers/Register/RegisterContainer.js
+++ containers/Register/RegisterContainer.js
@@ -4,7 +4,6 @@ import ButtonComponent from "../../components/Button/ButtonComponent";
 import { StyleSheet, View, Alert} from "react-native";
 import { SelectList } from "react-native-dropdown-select-list";
 import { colors } from "../../constants/colors";
-
 import User from "../../domain/models/user";
 
 
